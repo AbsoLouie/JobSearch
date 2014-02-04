330d976310a7602d184be8adbc162a3bc0fe2608
Louie Perez <louie.perez3@gmail.com>
2014-02-04 12:14:04 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobSearch::Application.config.secret_token = 'b21ca5d51874a73f2f7806579e5c7c0b618c32d2b4510ad0a5fa5d3484d4daa17bf0cbc4a55dc69eba2f2637d6f0dc0bf1657fbf705b74888566eb78f1d43d4d'
