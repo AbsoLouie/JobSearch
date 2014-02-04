be46fde3927ddb5b636c5efa710ddedf9bbda07a
Louie Perez <louie.perez3@gmail.com>
2014-02-04 15:49:24 -0800

    User Controller Create

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  attr_accessible :email, :username, :first_name, :last_name, :password
+
   validates :email, :username, :first_name, :last_name, :password, presence: true
   validates :email, :username, uniqueness: true
 
